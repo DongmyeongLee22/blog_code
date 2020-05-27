e7ea2c4f02658351e0bbe6b12f95c83166cbbab9
Dongmyeong Lee <dongmyeong.lee22@gmail.com>
2020-05-28 02:10:22 +0900

    VuePress :: 시작하기

---
+++ vuepress/blog/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    base: '/', // base url을 설정합니다.
+    title: 'VuePress 블로그',
+    head: [['link', {rel: 'icon', href: 'img.png'}]], // html head에 넣을 값들을 설정할 수 있습니다.
+    themeConfig: { // VuePress 기본 테마에 필요한 설정을을 추가합니다.
+        logo: '/vue.png',
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Sample', link: '/sample.html'},
+        ]
+    },
+};
