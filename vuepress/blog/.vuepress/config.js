d7837919d19de91aeda40de7e33b8a5f36bf5be4
Dongmyeong Lee <dongmyeong.lee22@gmail.com>
2020-05-30 21:28:15 +0900

    VuePress :: Travis 연동

--- vuepress/blog/.vuepress/config.js
+++ vuepress/blog/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base: '/', // base url을 설정합니다.
+    base: '/blog_code/', // base url을 설정합니다.
     title: 'VuePress 블로그',
     head: [['link', {rel: 'icon', href: 'img.png'}]], // html head에 넣을 값들을 설정할 수 있습니다.
     themeConfig: { // VuePress 기본 테마에 필요한 설정을을 추가합니다.
